a3689e4e4ed0e01eb6b860080d4469daefc2fb10
Medvedev Oleg <olegmedvmail@gmail.com>
2026-01-20 20:51:23 +0300

    Add initial project structure and core classes
    Created controllers, models, interfaces, and service classes for the JobPulse application. These include JobsController, Job, SearchRequest, IJobScraper, JobService, and LinkedInScraper as placeholders to establish the foundation for further development.

---
+++ JobPulse.Api/Controllers/JobsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobPulse.Api.Controllers
+{
+    public class JobsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
