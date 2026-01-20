eaffe7b748fed3eb89947c04a8a430e556e49cd0
Medvedev Oleg <olegmedvmail@gmail.com>
2026-01-20 21:17:35 +0300

    Switch to Swashbuckle for Swagger support
    Replaced Microsoft.AspNetCore.OpenApi with Swashbuckle.AspNetCore for Swagger documentation generation. Updated Program.cs to use AddSwaggerGen and configure Swagger UI in development. Modified launch settings to launch the browser at the Swagger UI by default.

--- JobPulse.Api/Program.cs
+++ JobPulse.Api/Program.cs
@@ -1,17 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();  // Нужно для Swagger
+builder.Services.AddSwaggerGen();             // Генерация документации
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();      // Включает /swagger/v1/swagger.json
+    app.UseSwaggerUI();    // Включает UI по адресу /swagger
 }
 
 app.UseHttpsRedirection();
